585de61d90a8035b6efa8ac61be9e2a81f316ac9
mmilushev91 <milushev_1990@abv.bg>
2025-05-14 13:48:02 +0300

    Update heroicInventory.js

--- Advanced/Objects and Composition - Exercise/heroicInventory.js
+++ Advanced/Objects and Composition - Exercise/heroicInventory.js
@@ -1,14 +1,13 @@
-
 function solve(data) {
   const heroes = [];
-  
   for (const line of data) {
     let [name, level, items] = line.split(" / ");
+    if (!name) {
+      continue;
+    }
     level = Number(level);
     items = items ? items.split(", ") : [];
     heroes.push({name, level, items})
   }
-  
   console.log(JSON.stringify(heroes))
-  
 }
