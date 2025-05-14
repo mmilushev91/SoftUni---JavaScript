17cac1fb13f71e5df6bbe2b2a9986907b789987f
mmilushev91 <milushev_1990@abv.bg>
2025-05-14 12:17:24 +0300

    Update carFactory.js

--- Advanced/Objects and Composition - Exercise/carFactory.js
+++ Advanced/Objects and Composition - Exercise/carFactory.js
@@ -1,55 +1,49 @@
 function solve(obj) {
   const car = {model: obj.model};
+  car.engine = pickEngine(obj.power);
+  car.carriage = pickCarriage(obj.carriage, obj.color);
+  car.wheels = calcWheelsize(obj.wheelsize);
+  return car;
   
-  const engines = {
-    "Small engine": { power: 90, volume: 1800 },
-    "Normal engine": { power: 120, volume: 2400 },
-    "Monster engine": { power: 200, volume: 3500 },
-  }
   
- 
-  
-  for (const eng in engines) {
-    const {power, volume} = engines[eng];
-    if (power >= obj.power) {
-      engine.power = power;
-      engine.volume = volume;
-      break  
+  //determines engine and volume and return obj with those values
+  function pickEngine(Objpower){
+    //1. Build object with power and volume types
+    const engines = {
+      "Small engine": { power: 90, volume: 1800 },
+      "Normal engine": { power: 120, volume: 2400 },
+      "Monster engine": { power: 200, volume: 3500 },
+    }
+    //2. Loop through the object to find engine with power >= to inputPower
+    
+    for (const engine in engines) {
+      const {power, volume} = engines[engine];
+      
+      if (power >= Objpower) {
+        //3. return object with engine and power according to the inputPower
+        return {
+          power,
+          volume
+        }
+      }
     }
   }
   
-  car[engine] = {engine};
-  console.log(car)
-  
-  // const carriage = {
-  //   Hatchback: { type: 'hatchback', color: <as required> },
-  //   Coupe: { type: 'coupe', color: <as required> }
-  // }
-  
-  const wheelsize = obj.wheelsize;
-  
-  if (wheelsize % 2 === 0) {
-    wheelsize -= 1;
+  //determines carriage and color and return obj with those values
+  function pickCarriage(carriage, color) {
+    return {
+      type: carriage,
+      color
+    }
   }
-  const wheels = Array(4).fill(wheelsize);
-
   
-  
-  
-  
-//   for
-  
-// console.log(car)
-
+  //calc wheelsize and return array with four wheels
+  function calcWheelsize(wheels) {
+    if (wheels % 2 === 0) {
+      wheels--;
+    }
+    
+    return Array(4).fill(wheels);
+  }
   
 }
-
-// Example usage:
-solve({ model: 'Opel Vectra',
-  power: 110,
-  color: 'grey',
-  carriage: 'coupe',
-  wheelsize: 17 }
-
-); // center
-
