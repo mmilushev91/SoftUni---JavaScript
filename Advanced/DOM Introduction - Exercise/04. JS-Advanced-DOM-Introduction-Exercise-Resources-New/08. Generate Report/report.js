f9b93c9f3625482dc41b5cd99e5c6049cba509b8
mmilushev91 <milushev_1990@abv.bg>
2025-05-20 10:53:50 +0300

    Update report.js

--- Advanced/DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/08. Generate Report/report.js
+++ Advanced/DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/08. Generate Report/report.js
@@ -1,3 +1,38 @@
 function generateReport() {
-    //TODO
+    
+    const headerColumns = document.querySelectorAll("input");
+    const columns = document.getElementsByTagName("td");
+    
+    const checkedIndexes = findCheckedIndexes(headerColumns);
+    console.log("length: ", columns.length)
+    const objArr = [];
+    for (let i = 0; i < columns.length; i += 7) {
+      
+      for (let j = 0; j < checkedIndexes.length; j++) {
+        const key = headerColumns[j].parentElement.textContent.toLowerCase();
+        console.log(i + checkedIndexes[j]);
+        
+        objArr.push({
+          
+        })
+      }
+    }
+
+    
+    // checkedIndexes.forEach((i) => {
+    //   headerColumns[i].parentElement.textContent = "Checked";
+    //   });
+
+    function findCheckedIndexes(iter) {
+      const indexes = [];
+      iter.forEach((el, i) => {
+        
+        if (el.checked) {
+          indexes.push(i);
+        }
+      });
+      
+      return indexes;
+    }
+    
 }
