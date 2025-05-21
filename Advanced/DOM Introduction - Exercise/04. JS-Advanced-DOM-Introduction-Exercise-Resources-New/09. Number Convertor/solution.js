33b82d0259be18e19ed07ca57aa6c378b28ea28f
mmilushev91 <milushev_1990@abv.bg>
2025-05-21 08:59:46 +0300

    Update solution.js

--- Advanced/DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/09. Number Convertor/solution.js
+++ Advanced/DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/09. Number Convertor/solution.js
@@ -1,4 +1,45 @@
 function solve() {
+  //Variables
+  const inputEl = document.getElementById("input");
+  const selectEl = document.getElementById("selectMenuTo");
+  const buttonEl = document.getElementsByTagName("button")[0]
+  const secondOptionEl = document.getElementsByTagName("option")[1]
+  const thirdOptionEl = createOptionEl();
+  const resultEl = document.getElementById("result");
+  
+  //insert Binary and Hexadecimal options to To select
+  secondOptionEl.value = "binary";
+  secondOptionEl.textContent = "Binary";
+  selectEl.appendChild(thirdOptionEl);
 
-    //TODO...
+  //listen for button clicks
+  buttonEl.addEventListener("click", function() {
+    const number = Number(inputEl.value);
+    
+    if (secondOptionEl.selected) {
+      resultEl.value = decimalToBinary(number);
+    } else {
+      resultEl.value = decimalToHex(number)
+    }
+    
+  })
+  //Create function for decimalToBinary
+  function decimalToBinary (num){
+    return num.toString(2);
+  } 
+  
+  //Create function for decimalToHex
+  function decimalToHex(num){
+    return num.toString(16).toUpperCase();
+  }    
+  //create function for new option element:
+  //-add it to select el
+  //-set its value to hexadecimal
+  //-set its textcontent Hexadecimal
+  function createOptionEl() {
+    const optionEl = document.createElement("option");
+    optionEl.value = "hexadecimal";
+    optionEl.textContent = "Hexadecimal";
+    return optionEl;
+  }
 }
